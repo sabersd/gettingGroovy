f142e4a964bdd83e21b68bdc2c45abccffcc9b97
sabersd <sabersd@gmail.com>
2014-03-04 23:09:44 -0600

    added the duckTyping examples

---
+++ src/duckTyping.groovy
@@ -0,0 +1,16 @@
+class duckTyping {
+	def now = new Date()
+	println now
+	println now.class
+
+	def file = new File("/Users/Doug/test.txt")
+	println file.class
+
+	def url = new URL('http://www.groovy.mn')
+	println url
+	println url.class
+
+	def list = new ArrayList(["first", "second", "third"])
+	println list
+	println list.class
+}
